0c52f93c7c6d12860b5616bd92613c586d1b0a7c
mvicariaf <amivicaria@hotmail.es>
2017-07-13 18:37:52 +0200

    paso 3 inicio

--- src/pizza.js
+++ src/pizza.js
@@ -1,25 +1,30 @@
 // Write your Pizza Builder JavaScript in this file.
 $( document ).ready(function() {
 
-//Seleccionar clase pep y aplicarle fadeToggle
-$('.btn-pepperonni').on('click', function(){
-	$('[class~=pep]').fadeToggle();
-});
-$('.btn-mushrooms').on('click', function(){
-	$('[class~=mushroom]').fadeToggle();
-});
-$('.btn-green-peppers').on('click', function(){
-	$('[class^=green-pepper]').fadeToggle();
-});
-$ ('.sauce').toggleClass('sauce-white',false);
-$ ('.btn-sauce').on('click', function(){
-	$('.sauce').toggleClass('sauce-white');
-});
+	//Seleccionar clase pep y aplicarle fadeToggle
+	$('.btn-pepperonni').on('click', function(){
+		$('[class~=pep]').fadeToggle();
+	});
+	$('.btn-mushrooms').on('click', function(){
+		$('[class~=mushroom]').fadeToggle();
+		if ($('section').hasClass('mushroom')== true){
+		$('btn-mushrooms').toggleClass('active');
+		}
+	});
+	$('.btn-green-peppers').on('click', function(){
+		$('[class^=green-pepper]').fadeToggle();
+	});
+	//$ ('.sauce').toggleClass('sauce-white',false);
+	$ ('.btn-sauce').on('click', function(){
+		$('.sauce').toggleClass('sauce-white');
+	});
+	$('section').removeClass('sauce-white');
+	$('section').removeClass('crust-gluten-free');
 
-$ ('.crust').toggleClass('crust-gluten-free',false);
-$ ('.btn-crust').on('click', function(){
-	$('.crust').toggleClass('crust-gluten-free');
-});
-
+	//$ ('.crust').toggleClass('crust-gluten-free',false);
+	$ ('.btn-crust').on('click', function(){
+		$('.crust').toggleClass('crust-gluten-free');
+	});
+	
 
 });
