23ecb5ca15ae4b5c75a156ff591f79483b3d180f
mvicariaf <amivicaria@hotmail.es>
2017-07-13 19:00:48 +0200

    paso 3 menos los dos ultimos botones

--- src/pizza.js
+++ src/pizza.js
@@ -4,26 +4,29 @@ $( document ).ready(function() {
 	//Seleccionar clase pep y aplicarle fadeToggle
 	$('.btn-pepperonni').on('click', function(){
 		$('[class~=pep]').fadeToggle();
+		$('.btn-pepperonni').toggleClass('active');
 	});
 	$('.btn-mushrooms').on('click', function(){
-		$('[class~=mushroom]').fadeToggle();
-		if ($('section').hasClass('mushroom')== true){
-		$('btn-mushrooms').toggleClass('active');
-		}
+		$('.mushroom').fadeToggle();
+		$('.btn-mushrooms').toggleClass('active');
 	});
 	$('.btn-green-peppers').on('click', function(){
 		$('[class^=green-pepper]').fadeToggle();
+		$('.btn-green-peppers').toggleClass('active');
 	});
 	//$ ('.sauce').toggleClass('sauce-white',false);
 	$ ('.btn-sauce').on('click', function(){
 		$('.sauce').toggleClass('sauce-white');
+		$('.btn-sauce').toggleClass('active', false);
 	});
 	$('section').removeClass('sauce-white');
 	$('section').removeClass('crust-gluten-free');
-
+	$('.btn-sauce').removeClass('active');
+	$('.btn-crust').removeClass('active');
 	//$ ('.crust').toggleClass('crust-gluten-free',false);
 	$ ('.btn-crust').on('click', function(){
 		$('.crust').toggleClass('crust-gluten-free');
+		$('.btn-crust').toggleClass('active');
 	});
 	
 
