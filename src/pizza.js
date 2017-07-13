fac33ad70fd1a2521e45f06fbaf720a427ff5caa
criscarguz <criscarguz@gmail.com>
2017-07-13 17:32:31 +0200

    toppings buttons done

--- src/pizza.js
+++ src/pizza.js
@@ -1,1 +1,19 @@
 // Write your Pizza Builder JavaScript in this file.
+$( document ).ready(function() {
+
+//Seleccionar clase pep y aplicarle fadeToggle
+$('.btn-pepperonni').on('click', function(){
+	$('[class~=pep]').fadeToggle();
+});
+$('.btn-mushrooms').on('click', function(){
+	$('[class~=mushroom]').fadeToggle();
+});
+$('.btn-green-peppers').on('click', function(){
+	$('[class^=green-pepper]').fadeToggle();
+});
+
+
+
+
+
+});
